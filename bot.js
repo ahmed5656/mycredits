3942b34ac711d92ebb71c2bdf8251c1160bd2c19
ahmed5656 <42041981+ahmed5656@users.noreply.github.com>
2018-09-07 19:16:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,16 @@ var channel = "487652334449590274";//ايدي الروم
     },305);
 })
 
+client.on('ready', async() => {
+var server = "487652334449590272"; // ايدي السررفر
+var channel = "487664652981895169";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**#daily **')
+    },1);
+})
+
+
+
 
 client.login(process.env.BOT_TOKEN); 
 
